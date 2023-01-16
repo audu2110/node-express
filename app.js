3182cc44b3fc4ee9b69ce8a214a5df8d33cf05b2
audu2110 <91955066+audu2110@users.noreply.github.com>
2023-01-16 23:53:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,3 +44,46 @@ app.use('/', (req, res, next) => {
 });
 
 app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><br><input type="text" name="size"><br><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    console.log(req.body.title);
+    console.log(req.body.size);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
